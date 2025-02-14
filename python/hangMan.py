8ebebe83045d61ad29f169908e89da47c1697d68
Joshua Chacko <Joshua.chacko.jc@gmail.com>
2025-02-14 00:19:59 -0600

    hangMan planning phase

---
+++ python/hangMan.py
@@ -0,0 +1,13 @@
+import random
+
+
+
+
+print("=========================")
+print("   WELCOME TO HANGMAN    ")
+print("=========================")
+
+#print(f"The Word contains ({len(word) * "_"})")
+
+while True:
+    
