6bfeb8c2f5a82c7bdaaff20bd78d4de52a4b5826
Joshua Chacko <joshua.chacko.jc@gmail.com>
2025-08-15 20:23:19 -0500

    Start Scrapping information from Book WEbsite

--- python/WebScrapper/books/books/items.py
+++ python/WebScrapper/books/books/items.py
@@ -8,5 +8,6 @@ import scrapy
 
 class BooksItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+        price = scrapy.Field()
+        title = scrapy.Field()
+        url = scrapy.Field()
